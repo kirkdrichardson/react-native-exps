2ca1149abc9d100765309e7cc11804b39d96b246
kirk <kirk.douglas.richardson@gmail.com>
2017-10-10 15:14:45 -0600

    pizza translator w/ mobx

--- App.js
+++ App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+import {observable, computed} from 'mobx';
+import {observer} from 'mobx-react';
+
+
+class TranslationText {
+  @observable text = '';
+  @computed get translation() {
+      return this.text.split(' ').map(word => word && '🍕').join(' ');
+  }
+}
+ const store = new TranslationText();
+
+
+@observer
+class App extends React.Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 5, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(input) => store.text = input}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {store.translation}
+        </Text>
       </View>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#222',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+export default App;
+
+
+//       <View style={{
+//         backgroundColor: '#222',
+//         flex: 1,
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//
+//         <Image style={{width: 80, height: 80, borderRadius: 50}}
+//           source={require('./assets/cookieMonster.jpg')}/>
+//
+//         <View style={styles.circle} />
+//         <View style={{ width: 80, height: 80, borderRadius: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+//
+//  const styles = StyleSheet.create({
+//       circle: {
+//         width: 80,
+//         height: 80,
+//         borderRadius: 50,
+//         backgroundColor: 'skyblue'
+//       }
+//  });
